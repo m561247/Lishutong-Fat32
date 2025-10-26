dae055edc104a9b4bbae971853a1edb2dcd70404
eason_wang <eason.wang@abilitycorp.com.tw>
2025-10-26 17:47:52 +0800

    1.01 code structure

--- xfat.c
+++ xfat.c
@@ -4,25 +4,6 @@
  * 课程网址：http://01ketang.cc
  * 版权声明：本源码非开源，二次开发，或其它商用前请联系作者。
  */
-#include <stdio.h>
-#include "xdisk.h"
 #include "xfat.h"
+#include "xdisk.h"
 
-static int array[32];
-
-void print_int (int v) {
-    printf("v = %d\n", v);
-}
-
-int main(void) {
-    int * p_array = array;
-
-    for (int i = 0; i < 100; i++) {
-        print_int(i);
-    }
-
-    p_array[0] = 32;
-
-    printf("Test End!\n");
-    return 0;
-}
